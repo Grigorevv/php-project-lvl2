d252ed815dc07af155fdf2d40881108edb5151e6
Viktor Grigorev <7184243@gmail.com>
2021-11-22 16:12:58 +0300

    Fatal error: Cannot redeclare Plain\Plain\iter()

--- src/formatters/plain.php
+++ src/formatters/plain.php
@@ -12,45 +12,45 @@ function toStr($value, $boolToStr)
     return is_array($value) ? '[complex value]' : "'{$value}'";
 }
 
+function iter($currentValue, $anchestry = [])
+{
+    $boolToStr = [true => 'true', null => 'null', false => 'false'];
+
+    $result = array_map(function ($item) use ($anchestry, $boolToStr) {
+        switch ($item['type']) {
+            case 'unchanged':
+                return '';
+
+            case 'added':
+                     $property = join('.', [...$anchestry, $item['key']]);
+                if (gettype($item['value']) === 'boolean' || $item['value'] === null) {
+                     $valueToStr = $boolToStr[$item['value']];
+                } else {
+                     $valueToStr = toStr($item['value'], $boolToStr);
+                }
+                return "Property '{$property}' was added with value: {$valueToStr}";
+
+            case 'deleted':
+                $property = join('.', [...$anchestry, $item['key']]);
+                return "Property '{$property}' was removed";
+
+            case 'changed':
+                $property = join('.', [...$anchestry, $item['key']]);
+                $valueToStrAfter = toStr($item['value2'], $boolToStr);
+                $valueToStrBefore = toStr($item['value'], $boolToStr);
+                return "Property '{$property}' was updated. From {$valueToStrBefore} to {$valueToStrAfter}";
+
+            case 'nested':
+                $result = iter($item['children'], [...$anchestry, $item['key']]);
+                return "{$result}";
+
+            default:
+                throw new \Exception("Unknown format: '{$item['type']}'!");
+        }
+    }, $currentValue);
+        return join("\n", array_filter($result, fn($item) => $item !== ''));
+}
 function renderPlain($ast)
 {
-    function iter($currentValue, $anchestry = [])
-    {
-        $boolToStr = [true => 'true', null => 'null', false => 'false'];
-
-        $result = array_map(function ($item) use ($anchestry, $boolToStr) {
-            switch ($item['type']) {
-                case 'unchanged':
-                    return '';
-
-                case 'added':
-                     $property = join('.', [...$anchestry, $item['key']]);
-                    if (gettype($item['value']) === 'boolean' || $item['value'] === null) {
-                        $valueToStr = $boolToStr[$item['value']];
-                    } else {
-                        $valueToStr = toStr($item['value'], $boolToStr);
-                    }
-                    return "Property '{$property}' was added with value: {$valueToStr}";
-
-                case 'deleted':
-                     $property = join('.', [...$anchestry, $item['key']]);
-                    return "Property '{$property}' was removed";
-
-                case 'changed':
-                     $property = join('.', [...$anchestry, $item['key']]);
-                     $valueToStrAfter = toStr($item['value2'], $boolToStr);
-                     $valueToStrBefore = toStr($item['value'], $boolToStr);
-                    return "Property '{$property}' was updated. From {$valueToStrBefore} to {$valueToStrAfter}";
-
-                case 'nested':
-                     $result = iter($item['children'], [...$anchestry, $item['key']]);
-                    return "{$result}";
-
-                default:
-                    throw new \Exception("Unknown format: '{$item['type']}'!");
-            }
-        }, $currentValue);
-        return join("\n", array_filter($result, fn($item) => $item !== ''));
-    }
     return iter($ast);
 }
